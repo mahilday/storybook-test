28a3c281c56dfd5108ab1b7ab552f3a6bd47e051
Mathilda Imadojiemu <mathilda.imadojiemu@mymdoc.com>
2022-02-15 14:45:46 +0100

    added scss variables and improved UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-
 import "./App.scss";
 import Form from "./containers/Form";
 
 import "./css/bootstrap.scss";
 
-
-
-
 function App() {
-
-  return (
-    <Form color="primary"/>
-      );
+  return <Form color="primary" />;
 }
 
 export default App;
